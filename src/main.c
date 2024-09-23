40f35aa759f2497ddee5e794bd7666bfaf56a9ac
DjordjevicPetar <dj.petar02@gmail.com>
2024-09-23 09:56:51 +0200

    1.2

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include "../inc/common.h"
 #include "../inc/chunk.h"
-#include <stdio.h>
+#include "../inc/debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+    dissasembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
 }
