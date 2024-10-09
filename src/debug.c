6c1bb9efa8fb27c559e756fa1bf0e07619b8187a
DjordjevicPetar <dj.petar02@gmail.com>
2024-10-09 17:35:37 +0200

    1.11

--- src/debug.c
+++ src/debug.c
@@ -96,6 +96,8 @@ int dissasebleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_PRINT", offset);
         case OP_LOOP:
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
+        case OP_CALL:
+            return byteInstruction("OP_CALL", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
