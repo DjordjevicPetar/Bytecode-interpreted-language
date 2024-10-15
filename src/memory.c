fed1fb7761a32c27557c73a4434e64cb29a12465
DjordjevicPetar <dj.petar02@gmail.com>
2024-10-15 20:10:56 +0200

    1.15

--- src/memory.c
+++ src/memory.c
@@ -72,7 +72,12 @@ static void freeObject(Obj* object) {
     #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD:
+            FREE(ObjBoundMethod, object);
+            break;
         case OBJ_CLASS:
+            ObjClass* klass = (ObjClass*)object;
+            freeTable(&klass->methods);
             FREE(ObjClass, object);
             break;
         case OBJ_CLOSURE:
@@ -119,6 +124,7 @@ static void markRoots() {
 
     markTable(&vm.globals);
     markCompilerRoots();
+    markObject((Obj*)vm.initString);
 }
 
 static void blackenObject(Obj* object) {
@@ -129,9 +135,16 @@ static void blackenObject(Obj* object) {
     #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod* bound = (ObjBoundMethod*)object;
+            markValue(bound->receiver);
+            markObject((Obj*)bound->method);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
+            markTable(&klass->methods);
             break;
         }
         case OBJ_CLOSURE: {
